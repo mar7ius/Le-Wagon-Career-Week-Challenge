d7867e6b6e7331af5a63782d6d4886c5edfb1ef4
Martial Salgues <github@mar7ius.me>
2021-09-09 11:55:10 +0200

    created museums controller, routes to show, and prepare for render JSON on call

---
+++ app/controllers/api/v1/museums_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::MuseumsController < Api::V1::BaseController
+  def show
+  end
+
+  private
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :museums, only: [:show]
+    end
+  end
 end
